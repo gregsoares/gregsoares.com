51848a84c9ba5eef0cfc50494a84c1e67715b0b5
gsoares <gsoares@wsgc.com>
2022-06-15 12:01:37 -0400

    all sections refactored into comonents

---
+++ src/components/Heroes.jsx
@@ -0,0 +1,162 @@
+const Heroes = () => {
+  return (
+    <section className="pt-20 pb-48">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-wrap justify-center text-center mb-24">
+          <div className="w-full lg:w-6/12 px-4">
+            <h2 className="text-4xl font-semibold">Here are our heroes</h2>
+            <p className="text-lg leading-relaxed m-4 text-gray-500">
+              According to the National Oceanic and Atmospheric Administration,
+              Ted, Scambos, NSIDClead scentist, puts the potentially record
+              maximum.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-wrap">
+          <div className="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
+            <div className="px-6">
+              <img
+                alt="..."
+                src="https://randompicturegenerator.com/img/people-generator/ge075537f0faab27382166ba2b020e03c0193bb9be263280bd5684b9e1853b1c3054e7cb39a94128a376dd9730e64b013_640.jpg"
+                className="shadow-lg rounded-full mx-auto max-w-120-px"
+              />
+              <div className="pt-6 text-center">
+                <h5 className="text-xl font-bold">Ryan Tompson</h5>
+                <p className="mt-1 text-sm text-gray-400 uppercase font-semibold">
+                  Web Developer
+                </p>
+                <div className="mt-6">
+                  <button
+                    className="bg-blue-400 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                  >
+                    <i className="fab fa-twitter"></i>
+                  </button>
+                  <button
+                    className="bg-blue-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                  >
+                    <i className="fab fa-facebook-f"></i>
+                  </button>
+                  <button
+                    className="bg-pink-500 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                  >
+                    <i className="fab fa-dribbble"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
+            <div className="px-6">
+              <img
+                alt="..."
+                src="https://randompicturegenerator.com/img/people-generator/ga3158913885a740b750dcf5b6319130e62bf9b22e8ae586b8ac40133bb5099661bf71696d824f2d96ba3ac4e029bc124_640.jpg"
+                className="shadow-lg rounded-full mx-auto max-w-120-px"
+              />
+              <div className="pt-6 text-center">
+                <h5 className="text-xl font-bold">Romina Hadid</h5>
+                <p className="mt-1 text-sm text-gray-400 uppercase font-semibold">
+                  Marketing Specialist
+                </p>
+                <div className="mt-6">
+                  <button
+                    className="bg-red-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                  >
+                    <i className="fab fa-google"></i>
+                  </button>
+                  <button
+                    className="bg-blue-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                  >
+                    <i className="fab fa-facebook-f"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
+            <div className="px-6">
+              <img
+                alt="..."
+                src="https://randompicturegenerator.com/img/people-generator/g0338942ae520fc2eadd70802251f03ed8e406e658afb686bc9fc5d17ffe2b19cfbab78e05c7cdac9646e0fa0314e51ee_640.jpg"
+                className="shadow-lg rounded-full mx-auto max-w-120-px"
+              />
+              <div className="pt-6 text-center">
+                <h5 className="text-xl font-bold">Alexa Smith</h5>
+                <p className="mt-1 text-sm text-gray-400 uppercase font-semibold">
+                  UI/UX Designer
+                </p>
+                <div className="mt-6">
+                  <button
+                    className="bg-red-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                  >
+                    <i className="fab fa-google"></i>
+                  </button>
+                  <button
+                    className="bg-blue-400 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                  >
+                    <i className="fab fa-twitter"></i>
+                  </button>
+                  <button
+                    className="bg-gray-700 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                  >
+                    <i className="fab fa-instagram"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
+            <div className="px-6">
+              <img
+                alt="..."
+                src="https://randompicturegenerator.com/img/people-generator/ge3d7bd2358e348fd52b1c5edb962a9212b3f0c27ea5bb2c14835f347c939f3977e78856207d4be1bf93911e38b349d88_640.jpg"
+                className="shadow-lg rounded-full mx-auto max-w-120-px"
+              />
+              <div className="pt-6 text-center">
+                <h5 className="text-xl font-bold">Jenna Kardi</h5>
+                <p className="mt-1 text-sm text-gray-400 uppercase font-semibold">
+                  Founder and CEO
+                </p>
+                <div className="mt-6">
+                  <button
+                    className="bg-pink-500 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                  >
+                    <i className="fab fa-dribbble"></i>
+                  </button>
+                  <button
+                    className="bg-red-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                  >
+                    <i className="fab fa-google"></i>
+                  </button>
+                  <button
+                    className="bg-blue-400 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                  >
+                    <i className="fab fa-twitter"></i>
+                  </button>
+                  <button
+                    className="bg-gray-700 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                  >
+                    <i className="fab fa-instagram"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Heroes;
