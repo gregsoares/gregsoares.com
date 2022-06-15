b4916a11c044383c3a9b31065bb4e9824e214b42
gsoares <gsoares@wsgc.com>
2022-06-15 10:58:20 -0400

    changed copyright, and added build to .gitignore

--- src/components/organisms/Footer/Footer.jsx
+++ src/components/organisms/Footer/Footer.jsx
@@ -144,12 +144,12 @@ const Footer = () => {
           <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4 mx-auto text-center">
               <div className="text-sm text-gray-500 font-semibold py-1">
-                Copyright © {new Date().getFullYear()} Notus React by{" "}
+                Copyright © {new Date().getFullYear()} by{" "}
                 <a
                   href="https://www.creative-tim.com?ref=nr-footer"
                   className="text-gray-500 hover:text-gray-800"
                 >
-                  Creative Tim
+                  Greg Soares
                 </a>
                 .
               </div>
