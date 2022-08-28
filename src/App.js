d6321379b6ebda58e4447fa99bb199e705a6abfb
Greg Soares <gregselvagem@gmail.com>
2022-08-27 23:19:15 -0400

    linting

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import TopNav from "./components/Molecules/TopNav";
-import Components from "./Pages/Components";
-import Home from "./Pages/Home/Home";
-import ToDo from "./Pages/ToDo/ToDo";
-import Footer from "./components/organisms/Footer/Footer";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import TopNav from './components/Molecules/TopNav'
+import Components from './Pages/Components'
+import Home from './Pages/Home/Home'
+import ToDo from './Pages/ToDo/ToDo'
+import Footer from './components/organisms/Footer/Footer'
 
 const App = () => (
   <Router>
     <TopNav />
     <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/Home" element={<Home />} />
-      <Route exact path="/todo" element={<ToDo />} />
-      <Route exact path="/Components" element={<Components />} />
+      <Route exact path='/' element={<Home />} />
+      <Route exact path='/Home' element={<Home />} />
+      <Route exact path='/todo' element={<ToDo />} />
+      <Route exact path='/Components' element={<Components />} />
     </Routes>
     <Footer />
   </Router>
-);
+)
 
-export default App;
+export default App
--- src/components/Molecules/TopNav/TopNav.js
+++ src/components/Molecules/TopNav/TopNav.js
@@ -1,60 +1,67 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
 const TopNav = () => {
-  const [isOpen, setIsOpen] = useState(window.innerWidth > 1023 ? true : false);
-
+  const location = useLocation().pathname
+  const [isOpen, setIsOpen] = useState(false)
+  console.log('isOpen', isOpen)
+  useEffect(() => {
+    setIsOpen(false)
+  }, [location])
   return (
-    <div className="text-white top-0 absolute z-50 w-full flex flex-wrap items-center px-2 py-3 navbar-expand-lg">
-      <div className="w-full px-4">
-        <nav className="relative flex flex-wrap items-center py-3 navbar-expand-lg rounded">
-          <div className="container px-2 mx-auto flex flex-wrap items-center justify-between">
-            <div className="w-full relative flex justify-between px-4 lg:static lg:flex ">
+    <div
+      className='text-white top-0 absolute z-50 w-full flex flex-wrap items-center px-2 py-3 navbar-expand-lg'
+      data-testid='topnav-container'
+    >
+      <div className='w-full px-4'>
+        <nav className='relative flex flex-wrap items-center py-3 navbar-expand-lg rounded'>
+          <div className='container px-2 mx-auto flex flex-wrap items-center justify-between'>
+            <div className='w-full relative flex justify-between px-4 lg:static lg:flex '>
               <Link
-                className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase "
-                to="/"
+                className='text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase '
+                to='/'
               >
                 GregSoares.com
               </Link>
 
-              <div className=" inline-block">
-                <div className="container mx-8">
+              <div className=' inline-block'>
+                <div className='container mx-8'>
                   <button
                     onClick={() => setIsOpen(!isOpen)}
-                    className="cursor-pointer text-xl leading-none px-3 py-3 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-                    type="button"
+                    className='cursor-pointer text-xl leading-none px-3 py-3 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none'
+                    type='button'
                   >
-                    <span className="block relative w-6 h-px rounded-sm bg-white"></span>
-                    <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
-                    <span className="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
+                    <span className='block relative w-6 h-px rounded-sm bg-white'></span>
+                    <span className='block relative w-6 h-px rounded-sm bg-white mt-1'></span>
+                    <span className='block relative w-6 h-px rounded-sm bg-white mt-1'></span>
                   </button>
                 </div>
                 {isOpen && (
                   <div
-                    className="flex lg:flex-grow items-center"
-                    id="example-navbar-info"
+                    className='flex lg:flex-grow items-center'
+                    id='example-navbar-info'
                   >
-                    <ul className="flex flex-col lg:flex-row list-none ml-auto">
-                      <li className="nav-item">
+                    <ul className='flex flex-col lg:flex-row list-none ml-auto'>
+                      <li className='nav-item'>
                         <Link
-                          className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
-                          to="/home"
+                          className='px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75'
+                          to='/home'
                         >
                           Home
                         </Link>
                       </li>
-                      <li className="nav-item">
+                      <li className='nav-item'>
                         <Link
-                          className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
-                          to="/components"
+                          className='px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75'
+                          to='/components'
                         >
                           Discover
                         </Link>
                       </li>
-                      <li className="nav-item">
+                      <li className='nav-item'>
                         <Link
-                          className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75"
-                          to="/about"
+                          className='px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug hover:opacity-75'
+                          to='/about'
                         >
                           About
                         </Link>
@@ -68,7 +75,7 @@ const TopNav = () => {
         </nav>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default TopNav;
+export default TopNav
