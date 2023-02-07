acc889a0441d927fe461ae30ce15928c94164d0f
Greg Soares <gregselvagem@gmail.com>
2023-02-06 19:35:15 -0500

    removed missed placeholders

--- src/components/Molecules/TopCardDisplay/TopCardDisplay.jsx
+++ src/components/Molecules/TopCardDisplay/TopCardDisplay.jsx
@@ -39,24 +39,27 @@ const CardSection = () => {
           {cardData.map((card, index) => (
             <Card key={index + 1} {...card} />
           ))}
-        </div>
+          {/* </div>
 
         <div className='flex flex-wrap items-center mt-32'>
           <div className='w-full md:w-5/12 px-4 mr-auto ml-auto'>
-            <div className='text-gray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white'>
+            <div className='text-gray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-blue-500'>
               <i className='fas fa-user-friends text-xl'></i>
             </div>
             <h3 className='text-3xl mb-2 font-semibold leading-normal'>
-              Working with us is a pleasure
+              From the Ground Up
             </h3>
-            <p className='text-lg font-light leading-relaxed mt-4 mb-4 text-gray-600'>
-              Don't let your uses guess by attaching tooltips and popoves to any
-              element. Just make sure you enable them first via JavaScript.
+            <p className='text-lg font-light leading-relaxed my-4 text-gray-600'>
+              One
             </p>
-            <p className='text-lg font-light leading-relaxed mt-0 mb-4 text-gray-600'>
-              The kit comes with three pre-built pages to help you get started
-              faster. You can change the text and images and you're good to go.
-              Just make sure you enable them first via JavaScript.
+            <p className='text-lg font-light leading-relaxed mb-4 text-gray-600'>
+              Two
+            </p>
+            <p className='text-lg font-light leading-relaxed mb-4 text-gray-600'>
+              Three
+            </p>
+            <p className='text-lg font-light leading-relaxed mb-4 text-gray-600'>
+              Conclusion
             </p>
           </div>
 
@@ -78,7 +81,7 @@ const CardSection = () => {
                 </p>
               </blockquote>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
--- src/components/organisms/Footer/Footer.jsx
+++ src/components/organisms/Footer/Footer.jsx
@@ -1,86 +1,86 @@
-import Link from "../../Atoms/Link/Link";
+import Link from '../../Atoms/Link/Link'
 
 const Footer = () => {
   return (
     <>
-      <footer className="relative bg-gray-200 pt-8 pb-6">
+      <footer className='relative bg-gray-200 pt-8 pb-6'>
         <div
-          className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20"
-          style={{ transform: "translateZ(0)" }}
+          className='bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20'
+          style={{ transform: 'translateZ(0)' }}
         >
           <svg
-            className="absolute bottom-0 overflow-hidden"
-            xmlns="http://www.w3.org/2000/svg"
-            preserveAspectRatio="none"
-            version="1.1"
-            viewBox="0 0 2560 100"
-            x="0"
-            y="0"
+            className='absolute bottom-0 overflow-hidden'
+            xmlns='http://www.w3.org/2000/svg'
+            preserveAspectRatio='none'
+            version='1.1'
+            viewBox='0 0 2560 100'
+            x='0'
+            y='0'
           >
             <polygon
-              className="text-gray-200 fill-current"
-              points="2560 0 2560 100 0 100"
+              className='text-gray-200 fill-current'
+              points='2560 0 2560 100 0 100'
             ></polygon>
           </svg>
         </div>
-        <div className="container mx-auto px-4">
-          <div className="flex flex-wrap text-center lg:text-left">
-            <div className="w-full lg:w-6/12 px-4">
-              <h4 className="text-3xl font-semibold">Let's keep in touch!</h4>
-              <h5 className="text-lg mt-0 mb-2 text-gray-600">
-                Find us on any of these platforms, we respond 1-2 business days.
+        <div className='container mx-auto px-4'>
+          <div className='flex flex-wrap text-center lg:text-left'>
+            <div className='w-full lg:w-6/12 px-4'>
+              <h4 className='text-3xl font-semibold'>Let's keep in touch!</h4>
+              <h5 className='text-lg mt-0 mb-2 text-gray-600'>
+                Thank you for visiting my website. I hope you find it useful.
               </h5>
-              <div className="mt-6 lg:mb-0 mb-6">
+              {/* <div className='mt-6 lg:mb-0 mb-6'>
                 <button
-                  className="bg-white text-blue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-                  type="button"
+                  className='bg-white text-blue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2'
+                  type='button'
                 >
-                  <i className="fab fa-twitter"></i>
+                  <i className='fab fa-twitter'></i>
                 </button>
                 <button
-                  className="bg-white text-blue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-                  type="button"
+                  className='bg-white text-blue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2'
+                  type='button'
                 >
-                  <i className="fab fa-facebook-square"></i>
+                  <i className='fab fa-facebook-square'></i>
                 </button>
                 <button
-                  className="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-                  type="button"
+                  className='bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2'
+                  type='button'
                 >
-                  <i className="fab fa-dribbble"></i>
+                  <i className='fab fa-dribbble'></i>
                 </button>
                 <button
-                  className="bg-white text-gray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-                  type="button"
+                  className='bg-white text-gray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2'
+                  type='button'
                 >
-                  <i className="fab fa-github"></i>
+                  <i className='fab fa-github'></i>
                 </button>
-              </div>
+              </div> */}
             </div>
-            <div className="w-full lg:w-6/12 px-4">
-              <div className="flex flex-wrap items-top mb-6">
-                <div className="w-full lg:w-4/12 px-4 mx-auto">
-                  <span className="block uppercase text-gray-500 text-sm font-semibold mb-2">
+            <div className='w-full lg:w-6/12 px-4'>
+              <div className='flex flex-wrap items-top mb-6'>
+                <div className='w-full lg:w-4/12 px-4 mx-auto'>
+                  <span className='block uppercase text-gray-500 text-sm font-semibold mb-2'>
                     Useful Links
                   </span>
-                  <ul className="list-unstyled">
+                  <ul className='list-unstyled'>
                     {/* Link */}
-                    <Link to="/" text="Home" />
+                    <Link to='/' text='Home' />
 
                     <Link
-                      href="https://www.linkedin.com/in/greg-soares/"
-                      text="LinkedIn"
+                      href='https://www.linkedin.com/in/greg-soares/'
+                      text='LinkedIn'
                       targetBlank
                     />
 
                     <Link
-                      href="https://github.com/gregsoares"
-                      text="Github"
+                      href='https://github.com/gregsoares'
+                      text='Github'
                       targetBlank
                     />
                     <Link
-                      href="https://github.com/gregsoares/gregsoares.com"
-                      text="GregSoares.com Repo"
+                      href='https://github.com/gregsoares/gregsoares.com'
+                      text='GregSoares.com Repo'
                       targetBlank
                     />
                   </ul>
@@ -127,14 +127,14 @@ const Footer = () => {
               </div>
             </div>
           </div>
-          <hr className="my-6 border-gray-300" />
-          <div className="flex flex-wrap items-center md:justify-between justify-center">
-            <div className="w-full md:w-4/12 px-4 mx-auto text-center">
-              <div className="text-sm text-gray-500 font-semibold py-1">
-                Copyright © {new Date().getFullYear()} by{" "}
+          <hr className='my-6 border-gray-300' />
+          <div className='flex flex-wrap items-center md:justify-between justify-center'>
+            <div className='w-full md:w-4/12 px-4 mx-auto text-center'>
+              <div className='text-sm text-gray-500 font-semibold py-1'>
+                Copyright © {new Date().getFullYear()} by{' '}
                 <a
-                  href="https://www.creative-tim.com?ref=nr-footer"
-                  className="text-gray-500 hover:text-gray-800"
+                  href='https://www.creative-tim.com?ref=nr-footer'
+                  className='text-gray-500 hover:text-gray-800'
                 >
                   Greg Soares
                 </a>
@@ -145,7 +145,7 @@ const Footer = () => {
         </div>
       </footer>
     </>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
