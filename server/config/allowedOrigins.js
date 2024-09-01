b7391091126633aae88c11d4aa9ffc87f464ceed
Yggdrasil <gregselvagem@gmail.com>
2024-09-01 17:54:34 -0400

    implemented server and api structure with additional TODOs (#15)

---
+++ server/config/allowedOrigins.js
@@ -0,0 +1,10 @@
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:8080',
+  'https://www.gregsoares.com',
+  'https://gregsoares.com',
+  'https://www.staging.gregsoares.com',
+  'https://staging.gregsoares.com'
+];
+
+module.exports = allowedOrigins;
