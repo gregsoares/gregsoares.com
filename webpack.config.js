28a91145b00d03df2ca67c0f8141b0a1368e4f35
Yggdrasil <gregselvagem@gmail.com>
2024-09-01 08:18:38 -0400

    pipeline: only push from ./dist after build & ++ Webback CssExtractPlugin (#8)
    * added github workflows for prod and staging
    
    * -apend
    
    * Feat: Added prod and staging workflows on push (#4)
    
    * added github workflows for prod and staging
    
    * -apend
    
    * fix server-dir targeting
    
    * Fix server-dir targeting (#5)
    
    * added github workflows for prod and staging
    
    * -apend
    
    * fix server-dir targeting
    
    * fix prod workflow target
    
    * Fixed prod workflow server-dir target (#6)
    
    * added github workflows for prod and staging
    
    * -apend
    
    * fix server-dir targeting
    
    * fix prod workflow target
    
    * fixed pipeline folder targets and created .cpanel
    
    * fixed pipeline folder targets and created .cpanel (#7)
    
    * added github workflows for prod and staging
    
    * -apend
    
    * fix server-dir targeting
    
    * fix prod workflow target
    
    * fixed pipeline folder targets and created .cpanel
    
    * added Steps to build and only deploy from dist, AND CssExtract for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,33 @@
 const path = require('path');
+const MiniCssExtractPluginLoader = require('mini-css-extract-plugin').loader;
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'index.bundle.js',
+    filename: 'index.bundle.js'
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, 'dist')
     },
     compress: true,
     port: 3000,
-    hot: true,
+    hot: true
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {loader: 'babel-loader'},
-        exclude: /node_modules/,
+        use: { loader: 'babel-loader' },
+        exclude: /node_modules/
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
+        MiniCssExtractPlugin: MiniCssExtractPluginLoader,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
 };
