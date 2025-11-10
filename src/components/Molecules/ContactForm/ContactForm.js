b71219844621df17237d304a416ff8a389937a38
Greg Soares <gregselvagem@gmail.com>
2025-11-10 14:17:49 -0500

    fix: deployment error after last merge

--- src/components/Molecules/ContactForm/ContactForm.js
+++ src/components/Molecules/ContactForm/ContactForm.js
@@ -61,19 +61,11 @@ const ContactForm = ({ successToast, failToast }) => {
             <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-200">
               <div className="flex-auto p-5 lg:p-10">
                 <h4 className="text-2xl font-semibold">Send a quick message</h4>
-<<<<<<< HEAD
                 <p className="leading-relaxed mt-1 mb-4 text-gray-800">
                   I'd love to hear from you.
                 </p>
                 <div className="relative w-full mb-3 mt-8">
                   <Label htmlFor="user_name" text="Name:" />
-=======
-                <p className="leading-relaxed mt-1 mb-4 text-gray-500">
-                  I'd love to hear from you.
-                </p>
-                <div className="relative w-full mb-3 mt-8">
-                  <Label htmlFor="user_name" text="Name" />
->>>>>>> master
                   <Input
                     hasError={error.user_name}
                     inputCompleted={(e) => handleInputCompleted(e)}
@@ -83,31 +75,19 @@ const ContactForm = ({ successToast, failToast }) => {
                 </div>
 
                 <div className="relative w-full mb-3">
-<<<<<<< HEAD
                   <Label htmlFor="user_email" text="Your Email:" />
-=======
-                  <Label htmlFor="user_email" text="Email" />
->>>>>>> master
 
                   <Input
                     hasError={error.user_email}
                     inputCompleted={(e) => handleInputCompleted(e)}
                     name="user_email"
                     type="email"
-<<<<<<< HEAD
                     placeholder="Your Email"
-=======
-                    placeholder="Email"
->>>>>>> master
                   />
                 </div>
 
                 <div className="relative w-full mb-3">
-<<<<<<< HEAD
                   <Label htmlFor="message" text="Message:" />
-=======
-                  <Label htmlFor="message" text="Message" />
->>>>>>> master
                   <Input
                     hasError={error.message}
                     inputCompleted={(e) => handleInputCompleted(e)}
